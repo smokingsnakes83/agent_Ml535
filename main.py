8705d5b69cd01c0b970e18ad65ad2fd42112e4ab
Smokingsnakes83 <smokingsnakes83@protonmail.com>
2024-07-02 12:07:12 -0300

    Update

--- main.py
+++ main.py
@@ -12,40 +12,17 @@ API_KEY = os.getenv("GOOGLE_API_KEY")  # 'REPLACE_BY_YOUR_API_HERE'
 genai.configure(api_key=API_KEY)
 
 # Load the CSV file into a pandas dataframe
-df = pd.read_csv("knowledge_base.csv")
-# df
+knowledge_base = pd.read_csv("knowledge_base.csv")
+
 
 # Defining the model in the model variable
 embed_model = "models/embedding-001"
 
-
-# Function that generates embeddings for documents
-def embed_doc(title, content):
-
-    ''''
-    Generates embeddings for documents using the specified template.
-
-    Args:
-        title: The title of the document.
-        content: The body of the document's content.
-
-    Returns:
-        An embedding vector representing the document.
-    '''
-    embedding = genai.embed_content(model=embed_model,
-                                content=content,
-                                task_type='retrieval_document',
-                                title=title)['embedding']
-
-    return embedding
-    
-# Apply the embed_doc function to each row of the dataframe
-df['Embeddings'] = df.apply(lambda row: embed_doc(row['Title'], row['Content']), axis=1)
-# df.to_csv("vector_database.csv", )
-# df
+# Load the embeddings
+embeddings = np.load("embeddings.npy", allow_pickle=True)
 
 # Function that generates query embeddings
-def embed_query(query, base, model, limit=0.7):
+def embed_query(query, limit=0.7):
     """
     Generates a semantic query and returns the corresponding information in the dataframe.
 
@@ -59,24 +36,22 @@ def embed_query(query, base, model, limit=0.7):
         The information corresponding to the query or an error message if the similarity is insufficient.
     """
 
-    query_embed = genai.embed_content(
-        model=model, content=query, task_type="retrieval_query"
+    query_embedding = genai.embed_content(
+        model=embed_model, content=query, task_type="retrieval_query"
     )["embedding"]
 
-    dot_products = np.dot(np.stack(df["Embeddings"]), query_embed)
+    dot_products = np.dot(np.stack(embeddings), query_embedding)
     idx = np.argmax(dot_products)
 
     # Calculate cosine similarity
-    similarity = dot_products[idx] / (
-        np.linalg.norm(query_embed) * np.linalg.norm(base["Embeddings"][idx])
-    )
+    similarity = dot_products[idx] / (np.linalg.norm(query_embedding) * np.linalg.norm(embeddings[idx]))
 
     # Check if the highest similarity is above the limit
     if similarity >= limit:
         print(f"\nsimilarity: {similarity}")
-        return df.iloc[idx]["Content"]
+        return knowledge_base.iloc[idx]["Content"]
 
-    elif similarity > 0.60:
+    elif similarity > 0.65:
         print(f"\nsimilarity: {similarity}")
         prompt = f"""Responda sobre {query} pela ótica de Mises e da Escola Austriaca de Economia.
                     Sugira perguntas relacionadas a Escola Austriaca de Economia"""
@@ -89,23 +64,24 @@ def embed_query(query, base, model, limit=0.7):
         return prompt
 
 
-# Model configurations and parameterization
-
+# Model configurations
 gen_config = {
-    'candidate_count':1,
-    'temperature':0.5,
-    'top_p': 0.7,
-    'top_k': 25
-}
+    "candidate_count": 1, 
+    "temperature": 0.5, 
+    "top_p": 0.7, 
+    "top_k": 25}
 
 safety_config = {
-    'harassment': 'block_none',
-    'hate': 'block_none',
-    'sexual': 'block_none',
-    'dangerous': 'block_none'
+    "harassment": "block_none",
+    "hate": "block_none",
+    "sexual": "block_none",
+    "dangerous": "block_none",
 }
 
+#######################################################################
 # Agent M|535
+
+# Function that displays a chat history in a Streamlit application.
 def chat_history(messages):
     """
     Displays a chat history in a Streamlit application.
@@ -120,6 +96,41 @@ def chat_history(messages):
     for message in messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
+
+# Function that processes a user query and embeds it
+def input_user_query(user_query, gen_model ):
+    """
+    Processes a user query, embeds it, generates a response using a language model, and returns the response.
+
+    Args:
+    user_query: The user's input query as a string.
+    gen_model: A language model object capable of generating text.
+
+    Returns:
+    A string representing the generated response to the user query.
+    """
+    passage = embed_query(user_query)
+
+    # Conditional for kids mode activation
+    if on:
+        st.caption(":boy: :green[Kids mode activated]")
+
+        prompt = f"""Este conteúdo é o seu conhecimento sobre Mises e a Escola Austríaca de Economia.
+            Você deve elaborar suas respostas sobre {query} baseadas nos conhecimentos adiquiridos
+            Utilize uma linguagem acessível.
+            De exemplos para concretizar a informação.
+            Você responderá sobre {query} com base em seus conhecimentos como se estivesse explicando para uma criança de 10 anos de idade.
+            Sua resposta deverá ser de fácil entendimento. {passage}"""
+    else:
+        prompt = f"""Este conteúdo é o seu conhecimento sobre Mises e a Escola Austríaca de Economia.
+            Você deve elaborar suas respostas sobre {query} baseadas nos conhecimentos adiquiridos
+            Utilize uma linguagem acessível.
+            De exemplos para concretizar a informação.
+
+            {passage}"""
+
+    response = gen_model.generate_content(prompt)
+    return response.text
 
 
 # Defining the page title and icon
@@ -146,44 +157,6 @@ with st.sidebar:
             """
     st.markdown(footer, unsafe_allow_html=True)
 
-# Function that processes a user query and embeds it
-def input_user_query(user_query, df, gen_model, embed_model):
-    """
-    Processes a user query, embeds it, generates a response using a language model, and returns the response.
-
-    Args:
-    user_query: The user's input query as a string.
-    df: A Pandas DataFrame containing relevant data for the query.
-    gen_model: A language model object capable of generating text.
-    embed_model: An embedding model used to embed the user query.
-
-    Returns:
-    A string representing the generated response to the user query.
-    """
-    passage = embed_query(user_query, df, embed_model)
-
-    # Conditional for kids mode activation
-    if on:
-        st.caption(":boy: :green[Kids mode activated]")
-
-        prompt = f"""Este conteúdo é o seu conhecimento sobre Mises e a Escola Austríaca de Economia.
-            Você deve elaborar suas respostas sobre {query} baseadas nos conhecimentos adiquiridos
-            Utilize uma linguagem acessível.
-            De exemplos para concretizar a informação.
-            Você responderá sobre {query} com base em seus conhecimentos como se estivesse explicando para uma criança de 10 anos de idade.
-            Sua resposta deverá ser de fácil entendimento. {passage}"""
-    else:
-        prompt = f'''Este conteúdo é o seu conhecimento sobre Mises e a Escola Austríaca de Economia.
-            Você deve elaborar suas respostas sobre {query} baseadas nos conhecimentos adiquiridos
-            Utilize uma linguagem acessível.
-            De exemplos para concretizar a informação.
-            {passage}'''
-
-    response = gen_model.generate_content(prompt, stream=True)
-    response.resolve()
-    return response.text
-
-
 # Message history initialization
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -202,16 +175,17 @@ if query:
     st.session_state.messages.append({"role": "You", "content": query})
 
     # Configure the generative model
-    gen_model = genai.GenerativeModel('gemini-1.5-flash',
-                                        generation_config=gen_config,
-                                        safety_settings=safety_config,
-                                        system_instruction="""
-                                        Voce é um agente expecialista em Mises e escola Austriaca de economia, seu nome é M|535
-                                        Você sempre deve saudar o usuário"""
-                                        )
+    gen_model = genai.GenerativeModel(
+        "gemini-1.5-flash",
+        generation_config=gen_config,
+        safety_settings=safety_config,
+        system_instruction="""Voce é um agente expecialista em Mises e escola Austriaca de economia, seu nome é M|535
+                            Este conteúdo é o seu conhecimento sobre Mises e a Escola Austríaca de Economia.
+                            Você sempre deve saudar o usuário""",
+    )
 
     # Process the query and get the response
-    response = input_user_query(query, df, gen_model, embed_model)
+    response = input_user_query(query, gen_model)
 
     # Display the response
     with st.chat_message(avatar="assets/bot.png", name="Agent M|535"):
