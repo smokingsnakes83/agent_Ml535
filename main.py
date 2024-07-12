192d69f766bd1a1168a32f504c212fd0cc3baad4
Smokingsnakes83 <smokingsnakes83@protonmail.com>
2024-07-12 20:51:55 -0300

    Update

--- main.py
+++ main.py
@@ -80,6 +80,7 @@ safety_config = {
 #######################################################################
 # Agent M|535
 
+
 # Function that displays a chat history in a Streamlit application.
 def chat_history(messages):
     """
@@ -149,13 +150,71 @@ with st.sidebar:
 
     # Page footer
     footer = """
-        <footer style="position: fixed; right: 0; bottom: 0; width: 100%; height: 5%; background-color: #171717; padding: 4px; text-align: left;">
+        <footer style="position: fixed; left: 0; bottom: 0; width: 20%; height: 5%;  padding: 4px; text-align: left;">
             <div class="footer-content">
-                <p style="font-size: 0.875em; color: #4c5666; text-align: center;">Agent M|535 by SmokingSnakes83</p>
+                <p style="font-size: 0.875em; color: #4c5666; text-align: left;">Agent M|535 by SmokingSnakes83</p>
             </div>
         </footer>
             """
     st.markdown(footer, unsafe_allow_html=True)
+
+
+# List of suggested questions
+questions_list = [
+    "O que distingue a ação humana do comportamento animal, de acordo com Mises?",
+    "Quais são os pré-requisitos para a ação humana?",
+    "Qual a relação entre a praxeologia e a psicologia?",
+    "O que é o apriorismo e por que Mises o considera fundamental para a economia?",
+    "Explique o princípio do individualismo metodológico.",
+    "Qual o papel da razão na ação humana?",
+    "Como o polilogismo contesta a lógica e a razão?",
+    "Por que a teoria do cálculo econômico é crucial para Mises?",
+    "Quais as principais características da economia de mercado?",
+    "O que é cataláxia?",
+    "Qual a diferença entre bens livres e bens econômicos?",
+    "O que define a utilidade marginal?",
+    "Explique a lei dos rendimentos decrescentes.",
+    "O que distingue o trabalho introvertido do trabalho extrovertido?",
+    "Quais são as fontes de prazer no trabalho?",
+    "Como a economia de mercado funciona em relação ao tempo?",
+    "O que é preferência temporal e qual a sua importância?",
+    "Quais as diferentes formas de poupança e como elas se relacionam com o capital?",
+    "Explique a natureza do juro originário.",
+    "Quais os principais componentes da taxa bruta de juros do mercado?",
+    "Como a expansão do crédito afeta a taxa de juros?",
+    "Como Mises explica a relação entre a oferta de moeda e o poder aquisitivo?",
+    "O que é o teorema da regressão e qual a sua importância?",
+    "Quais os diferentes tipos de moeda e quais as suas características?",
+    "Como Mises analisa o surgimento da moeda fiduciária?",
+    "O que é o padrão-ouro e quais suas vantagens?",
+    "Explique o conceito de " "entesouramento" " e por que Mises o considera um mito.",
+    "Como a intervenção governamental no sistema monetário gera caos?",
+    "O que é o problema do cálculo econômico no socialismo?",
+    "Quais as principais falhas do planejamento centralizado?",
+    "Qual o papel do governo em uma sociedade livre, segundo Mises?",
+    "Explique a falácia da " "economia mista" ".",
+    "Quais as consequências do controle de preços?",
+    "Como o intervencionismo leva ao socialismo?",
+    "Quais os diferentes tipos de intervenção fiscal?",
+    "Explique a natureza e os efeitos das medidas restritivas à produção.",
+    "Por que a busca por preços " "justos" " é uma ilusão?",
+    "Quais as características dos preços monopolísticos?",
+    "Qual a diferença entre lucro empresarial e ganho monopolístico?",
+    "Por que Mises considera o sindicalismo um sistema ineficaz e prejudicial?",
+    "Como a doutrina do " "efeito de Ricardo" " é equivocada?",
+    "Qual o papel da especulação em uma economia de mercado?",
+    "Qual o significado do conceito de " "capital" " para Mises?",
+    "Explique a importância da propriedade privada para o funcionamento do livre mercado.",
+    "Por que Mises considera a guerra e o socialismo como sistemas incompatíveis com a liberdade e a prosperidade?",
+    "Quais os principais problemas da " "economia de guerra" "?",
+    "Como o intervencionismo governamental gera a mentalidade anticapitalista?",
+    "Quais os principais argumentos em favor da "
+    "estabilização"
+    " e por que Mises os considera falaciosos",
+    "Qual o papel da educação na difusão do pensamento econômico?",
+    "Por que Mises acredita que o estudo da economia é crucial para o futuro da liberdade?",
+]
+questions_samples = random.choice(questions_list)
 
 # Message history initialization
 if "messages" not in st.session_state:
@@ -165,7 +224,7 @@ if "messages" not in st.session_state:
 chat_history(st.session_state.messages)
 
 # Capture user query
-query = st.chat_input("Ask Agent Mi535:")
+query = st.chat_input("Pergunte ao Mi535")
 
 if query:
     with st.chat_message(name="You"):
@@ -192,84 +251,18 @@ if query:
         st.markdown(response)
     # Add the answer to the history
     st.session_state.messages.append({"role": "M|535", "content": response})
-    
+
 #########################################################
-#Start screen
-
-questions_list_1 = [
-    "O que distingue a ação humana do comportamento animal, de acordo com Mises?",
-    "Quais são os pré-requisitos para a ação humana?",
-    "Qual a relação entre a praxeologia e a psicologia?",
-    "O que é o apriorismo e por que Mises o considera fundamental para a economia?",
-    "Explique o princípio do individualismo metodológico.",
-    "Qual o papel da razão na ação humana?",
-    "Como o polilogismo contesta a lógica e a razão?",
-    "Por que a teoria do cálculo econômico é crucial para Mises?",
-    "Quais as principais características da economia de mercado?",
-    "O que é cataláxia?",
-    "Qual a diferença entre bens livres e bens econômicos?",
-    "O que define a utilidade marginal?",
-    "Explique a lei dos rendimentos decrescentes.",
-    "O que distingue o trabalho introvertido do trabalho extrovertido?",
-    "Quais são as fontes de prazer no trabalho?",
-    "Como a economia de mercado funciona em relação ao tempo?",
-    "O que é preferência temporal e qual a sua importância?",
-    "Quais as diferentes formas de poupança e como elas se relacionam com o capital?",
-    "Explique a natureza do juro originário.",
-    "Quais os principais componentes da taxa bruta de juros do mercado?",
-    "Como a expansão do crédito afeta a taxa de juros?",
-    "Como Mises explica a relação entre a oferta de moeda e o poder aquisitivo?",
-    "O que é o teorema da regressão e qual a sua importância?",
-    "Quais os diferentes tipos de moeda e quais as suas características?",
-    "Como Mises analisa o surgimento da moeda fiduciária?",]
-questions_list_2 = [
-    "O que é o padrão-ouro e quais suas vantagens?",
-    "Explique o conceito de ""entesouramento"" e por que Mises o considera um mito.",
-    "Como a intervenção governamental no sistema monetário gera caos?",
-    "O que é o problema do cálculo econômico no socialismo?",
-    "Quais as principais falhas do planejamento centralizado?",
-    "Qual o papel do governo em uma sociedade livre, segundo Mises?",
-    "Explique a falácia da ""economia mista"".",
-    "Quais as consequências do controle de preços?",
-    "Como o intervencionismo leva ao socialismo?",
-    "Quais os diferentes tipos de intervenção fiscal?",
-    "Explique a natureza e os efeitos das medidas restritivas à produção.",
-    "Por que a busca por preços ""justos"" é uma ilusão?",
-    "Quais as características dos preços monopolísticos?",
-    "Qual a diferença entre lucro empresarial e ganho monopolístico?",
-    "Por que Mises considera o sindicalismo um sistema ineficaz e prejudicial?",
-    "Como a doutrina do ""efeito de Ricardo"" é equivocada?",
-    "Qual o papel da especulação em uma economia de mercado?",
-    "Qual o significado do conceito de ""capital"" para Mises?",
-    "Explique a importância da propriedade privada para o funcionamento do livre mercado.",
-    "Por que Mises considera a guerra e o socialismo como sistemas incompatíveis com a liberdade e a prosperidade?",
-    "Quais os principais problemas da ""economia de guerra""?",
-    "Como o intervencionismo governamental gera a mentalidade anticapitalista?",
-    "Quais os principais argumentos em favor da ""estabilização"" e por que Mises os considera falaciosos",
-    "Qual o papel da educação na difusão do pensamento econômico?",
-    "Por que Mises acredita que o estudo da economia é crucial para o futuro da liberdade?"
-    ]
-    
-question_sample_1 = random.choice(questions_list_1)
-question_sample_2 = random.choice(questions_list_2)
-
-if not query:
-    col11, col21, col31 = st.columns(3)
-    with col11:
-        st.container()
-    with col21:
-            st.image("assets/logo.png", width=280)
-    with col31:
-        st.container()
-            
+# Start screen
+else:
     col1, col2, col3 = st.columns(3)
-    
     with col1:
-        with st.container(border=True, height=135):
-            st.markdown("Estou pronto para te ajudar a entender o mundo pela ótica da Escola Austríaca de Economia (EAE).")
+        with st.container():
+            st.markdown("")
     with col2:
-        with st.container(border=True, height=135):
-            st.write("Pergunte-me:",question_sample_1)
+        with st.container():
+            st.image("assets/logo.png", caption=questions_samples)
     with col3:
-        with st.container(border=True, height=135):
-            st.write("Pergunte-me:", question_sample_2)
+        with st.container():
+            st.markdown("")                
+            
